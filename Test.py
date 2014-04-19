38e360c075d11b4bf2d8cbd7ff690be0f737f29c
maxgerar <maxgerar@ulb.ac.be>
2014-04-19 17:05:42 +0200

    amelioration fonction coloriage

--- Test.py
+++ Test.py
@@ -29,37 +29,46 @@ class ImgSegmentation:
     
     def segmente(self):
         
-        #lecture de l'image vers un ndarray
+        #lecture de l'image vers un ndarray. toutes les images fournies ont une taille de 12051000 pixels.
         im = imread(self.name)
-        print im.size
-        #on filtre pour enlever le bruit et avant d'echantillonner pour eviter le repliement spectral
-        #plus le rayon du disque est important plus le lissage est fort
+        
+        #Filtrage median pour enlever le bruit et eviter le repliement spectral en cas d'echantillonnage. Point positif : c'est un filtre qui conserve les bords !!!
+        #plus le rayon du disque est important plus le lissage est fort et plus il y a risque de perte d'information, le filtre supprimant les details.
         im = rank.median(im,disk(8))
         
         #on reduit l'image pour diminuer le temps de computation. On s'interesse qu'a certaines parties de l'image plus echantillonnage. C'est l'image grayscale
-        self.im_red = im[400:3000:2,500:4000:2]
+        self.im_red = im[100:5000:2,300:3000:2]
         self.segments = (self.im_red.size)/550
         
+        
         # slic attend une image rgb il faut donc faire la conversion
-        # cette fonction attend egalement des valeurs d'inteniste en float
-        self.img_temp = gray2rgb(self.im_red)
+        self.img_temp= gray2rgb(self.im_red)
+        
+        # cette fonction attend egalement des valeurs d'intensite en float
         self.img = img_as_float(self.img_temp)
         
-        #segmentation en superpixel
+        #segmentation en superpixel, fonction slic de skimage.segmentation
         self.segments_slic = slic(self.img,n_segments = self.segments,compactness = self.compacite, sigma =1)
         
+#        print self.segments_slic.shape
+#        print find_boundaries(self.segments_slic).shape
+
         #on doit avoir tous les indices sup a 0 pour region props donc on les augmente tous de 1
         self.segments_slic = self.segments_slic + np.ones(self.segments_slic.shape,dtype = np.int64)
         
         
         # liste de proprietes par superpixel
         self.props = regionprops(self.segments_slic,intensity_image = self.im_red)
-        # on crer une liste python apd de regioprops
-        self.pix_liste = list(self.props)
-        print len(self.pix_liste)
         
+        #on creer une liste contenant tout les labels des superpixels
+        self.regionlabels = list ()
+        for elem in self.props:
+            self.regionlabels.append(elem.label)
+        
+
         #liste qui permettra de stocker les labels des superpixel qui ont ete colore
         self.colored_pixel_label = list()
+        
         
         # Liaison de click avec la fonction onclick
         self.fig = plt.figure('segmentation')
@@ -67,34 +76,49 @@ class ImgSegmentation:
 
         
         #Affichage
-        self.affichage(mark_boundaries(self.img,self.segments_slic)) 
+        self.affichage(mark_boundaries(self.img,self.segments_slic))
     
     
     #fonction a lancer si clic de souris
     def onclick(self,event):
 
         
-        #on veut changer la couleur du superpixel sur lequel on vient de cliquer
-        
-#        #identification du superpixel clicke
+        #identification du superpixel clicke
         superpixel = self.segments_slic[event.ydata,event.xdata]
         
         # si le pixel a deja ete colorie on le decolorie
+        
         if self.props[superpixel-1].label in self.colored_pixel_label:
-           image = img_as_float(self.img_temp)
             # on le retire de la liste des pixel colorie
            self.colored_pixel_label.remove(self.props[superpixel-1].label)
+           
+           #on recupere les valeurs d'origine
+           image = img_as_float(self.img_temp)
+           
             #on decolorie le superpixel
-            #on recupere les donnees d'origine
            for row in self.props[superpixel-1].coords:
                self.img[row[0],row[1],0]=image[row[0],row[1],0]
                self.img[row[0],row[1],1]=image[row[0],row[1],1]
                self.img[row[0],row[1],2]=image[row[0],row[1],2]
-        #sinon on le colorie lui et c'est voisins
+    
+        #sinon on le colorie lui et ses voisins
         else:
             
+            #on colorie le pixel clique et on l'indique dans la liste
+            self.color_superpixel(self.props[superpixel-1].coords)
+            self.colored_pixel_label.append(self.props[superpixel-1].label)
+            
             #fonction permettant de colorier les superpixels semblables appartenant a l'elem designe par l'utilisateur
-            self.color_expand(superpixel-1,self.pix_liste)
+            self.color_expand(superpixel-1)
+        
+            #fonction permettant d'etendre ca
+            #pour tout les pixels du voisinage de celui qu'on a colorie on relance la mm fonction sur la liste de ces voisins
+#            for elem in self.colored_temp:
+#                self.color_expand(elem-1,self.regionlabels)
+#            
+#            #on remet a null la liste des voisins en attendant le prochain clik
+#            self.colored_temp=[]
+
     
         #affichage des statistiques sur le superpixel clique
 #        print median(self.props[superpixel-1].coords)
@@ -117,39 +141,40 @@ class ImgSegmentation:
 
     
     
-    #fonction recursive qui va colorier l'elem designe par l'utilisateur
-    def color_expand(self,indice,liste):
+    #fonction recursive qui va colorier l'elem designe par l'utilisateur et ses voisins qui lui sont suffisamment semblables
+    def color_expand(self,indice):
         
         #param du superpixel dont on etudie le voisinage
         centre1 = self.props[indice].centroid
         mediane1 = self.mediane(self.props[indice].coords)
-        self.color_pixel(self.props[indice].coords)
-        self.colored_pixel_label.append(self.props[indice].label)
-        
-        for elem in liste:
-            #calcul de distance entre les centroides des superpixels
-            centre2 = elem.centroid #le centre "mobile"
-            distance = math.sqrt(((centre1[0]-centre2[0])**2)+((centre1[1]-centre2[1])**2))
+
+        #comparaison avec les autres superpixels de liste. liste est la liste des labels de superpixels concernes.
+        for elem in self.regionlabels:
             
-            # Test pour savoir si les superpixels sont voisins
-            if distance <=30:
+            if elem not in self.colored_pixel_label:
+
+                #calcul de distance entre les centroides des superpixels
+                centre2 = self.props[elem-1].centroid #le centre "mobile"
+                distance = math.sqrt(((centre1[0]-centre2[0])**2)+((centre1[1]-centre2[1])**2))
+            
+                # Test pour savoir si les superpixels sont voisins
+                if distance <=30:
                
-            #maintenant on test la similarite en intensite via les mediane de distrib d'intensite des superpixels
+               #maintenant on test la similarite en intensite via les mediane de distrib d'intensite des superpixels
                
-               mediane2 = self.mediane(elem.coords)
-               diff = math.fabs(mediane1-mediane2)
+                    mediane2 = self.mediane(self.props[elem-1].coords)
+                    diff = math.fabs(mediane1-mediane2)
               
-               if diff <=15:
-            #on colorie le superpixel teste
-                  self.color_pixel(elem.coords)
-            #on indique dans une liste qu'on a colorie ce superpixel
-                  self.colored_pixel_label.append(elem.label)
-               #on recupere l'indice du superpixel dans la liste props
-                  index = (elem.label)-1
-                #on retire le superpixel colorie de la liste
-                  liste.pop(liste.index(elem))
-                #on rapelle la methode sur la liste
-                  self.color_expand(index,liste)
+                    if diff <=12:
+                        #on colorie le superpixel teste
+                        self.color_superpixel(self.props[elem-1].coords)
+                        #on indique dans une liste qu'on a colorie ce superpixel et on l'indique dans la liste des voisins
+                        self.colored_pixel_label.append(elem)
+                        #self.colored_temp.append(elem)
+                        #recursivite
+                        self.color_expand(elem-1)
+
+
 
 
 
@@ -159,8 +184,8 @@ class ImgSegmentation:
         plt.show()
     
     
-    
-    def color_pixel(self,coords):
+    #fonction permettant de colorer un superpixel
+    def color_superpixel(self,coords):
         for row in coords:
             self.img[row[0],row[1],0]=30
             self.img[row[0],row[1],1]=144
@@ -216,6 +241,6 @@ class ImgSegmentation:
 
 
 if __name__ == "__main__":
-    Im = ImgSegmentation('30.1.foto180b.12000x.TIF')
+    Im = ImgSegmentation('30.1.foto177a.12000x.TIF')
     Im.segmente()
     
